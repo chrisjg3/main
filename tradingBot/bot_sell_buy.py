86a0d9e91523279b06ef166a8830dbca77d6553b
Chris Joseph Gallo <chrisjg3@users.noreply.github.com>
2020-06-04 00:31:10 -0400

    Trade Bot Fully Functional

--- tradingBot/bot_sell_buy.py
+++ tradingBot/bot_sell_buy.py
@@ -1,44 +1,46 @@
 import pandas as pd
-import pandas_datareader.data as web
-import datetime as dt
-i = 0
+# import datetime as dt
+# from datetime import date
+# import pandas_datareader.data as web
+# i = 0
 
-pd.options.mode.chained_assignment = None
-port = pd.read_csv('tradingBot/test_stock.csv')
-
-# fifty_mma = [1250, 662, 134, 400]
-# hundred_mma = [1107, 602, 172, 391]
-
-# port['50ma'] = fifty_mma
-# port['100ma'] = hundred_mma
+# pd.options.mode.chained_assignment = None
+# port = pd.read_csv('tradingBot/test_stock.csv')
 
 
-hundred_days = dt.timedelta(100)
-fifty_days = dt.timedelta(50)
-end = dt.datetime.now()
-start = end - hundred_days
-money_sunk = -10000.0   # A better name for this might be limit, as we are setting it as an investment limit. Also it is negative
+# port['50ma'] = 0
+# port['100ma'] = 0
 
-for each in port['stock']:
-    df = web.DataReader(each, 'yahoo', start, end)['Adj Close']
-    df = df.drop(columns=['Date'])
-    port['100ma'][i] = df.mean()
-    df = df.tail(50)
-    port['50ma'][i] = df.mean()
-    i += 1
+# print(port)
 
-print("\n\n\n")
-print(port)
-print("\n\n")
+# hundred_days = dt.timedelta(100)
+# fifty_days = dt.timedelta(50)
+# end = dt.datetime.now()
+# start = end - hundred_days
+
+# "money_sunk" essentially is the max the bot will be allowed to invest per stock.
+# This will appear in the 'current_invest' column
+# money_sunk = -10000.0  
+
+# for each in port['stock']:
+#     df = web.DataReader(each, 'yahoo', start, end)['Adj Close']
+#     df = df.drop(columns=['Date'])
+#     port['100ma'][i] = df.mean()
+#     df = df.tail(50)
+#     port['50ma'][i] = df.mean()
+#     i += 1
+
+# print("\n\n\n")
+# print(port)
+# print("\n\n")
 
 
 
 
-def buy_stock(port):
-    global money_sunk
+def buy_stock(port, money_sunk):
     which_index = 0
     for each in port['100ma']:
-        if each * 1.05 <= port['50ma'][which_index]:
+        if each * 1.02 <= port['50ma'][which_index]:
             # If a stock has a signficantly higher 50ma then 100ma, we will buy, but only if not at limit.
             if port['current_invest'][which_index] > money_sunk:
                 room_to_invest = port['current_invest'][which_index] - money_sunk
@@ -55,18 +57,12 @@ def sell_stock(port):
     which_index = 0
     for each in port['50ma']:
         # If stock is slightly below, it auto sells.
-        if each * 1.03 <= port['100ma'][which_index]:
+        if each * 1.01 <= port['100ma'][which_index]:
             if port['quantity'][which_index] >= 1:
                 port['current_invest'][which_index] = port['current_invest'][which_index] + (port['quantity'][which_index] * port['live_price'][which_index])
                 port['quantity'][which_index] = 0
         which_index += 1
     return port
 
-
-port = buy_stock(port) 
-port = sell_stock(port)
-
-print("\n\n This is the manipulated one: \n\n")
-print(port)
 
 # port.to_csv('tradingBot/test_stock.csv', index=False)
